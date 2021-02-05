898985d25fc0ed1a66e13f15e9e1d14f794b0c34
Jrsparl <john@sparlingmusic.com>
2021-02-05 17:25:30 -0500

    initial commit with backend refactored

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,50 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+  
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  
+  
+  
+  type Query {
+    me: User
+  }
+
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(body: saveBookInput): User
+    removeBook(bookId: String!): User
+  }
+  input saveBookInput {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
